18ecba92727573f340cd738749ad2f7268b71e53
Piyachat Leelasilapasart <37095103+LeePiyachat@users.noreply.github.com>
2020-03-05 11:46:32 +1100

    change the pattern follow Julia

--- src/StochasticGrid.jl
+++ src/StochasticGrid.jl
@@ -1,9 +1,9 @@
-function StochasticGrid(Gridsize::Int64,Initpoint::Vector{Float64},Length::Int64,Pathnumber::Int64,X::rcss)
-    return(StochasticGrid(Gridsize,Initpoint,Length,Pathnumber,X.disturb,X.Weight))
+function stochasticgrid(gridsize::Int64,initpoint::Vector{Float64},length::Int64,pathnumber::Int64,x::rcss)
+    return(stochasticgrid(gridsize,initpoint,length,pathnumber,x.disturb,x.weight))
 end
 ############################################################################
-function StochasticGrid(Gridsize::Int64, Initpoint::Vector{Float64},Length::Int64, Pathnumber::Int64, disturb::Array{Float64,3}, Weight::Vector{Float64})
-    path = SimulatePath(Initpoint,Length::Int64,Pathnumber::Int64,disturb,Weight)[1]
-    Y = kmeans(path, Gridsize; maxiter = 200, display=:iter)
+function stochasticgrid(gridsize::Int64, initpoint::Vector{Float64},length::Int64, pathnumber::Int64, disturb::Array{Float64,3}, weight::Vector{Float64})
+    path = simulatepath(initpoint,length::Int64,pathnumber::Int64,disturb,weight)[1]
+    Y = kmeans(path, gridsize; maxiter = 200, display=:iter)
     return(transpose(Y.centers))
 end
