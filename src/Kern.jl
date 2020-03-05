45fb34d73f49141e506091b1e7ce13bd5f0c1ffd
Piyachat Leelasilapasart <37095103+LeePiyachat@users.noreply.github.com>
2020-03-05 12:04:13 +1100

    Add files via upload

---
+++ src/Kern.jl
@@ -0,0 +1,25 @@
+function Kern(distances::Vector{Float64},X)
+    ex = X.mp["ex"][1]
+    minndx = argmin(distances)
+    minval = distances[minndx]
+    result = zero(distances)
+
+    if minval < 0
+        error("Distance in kernel is negative! = ", minva)
+        result[minndx] = 1
+    else
+        if minval < 0.0000000001
+            result[minndx] = 1
+        else
+            for i in 1:length(distances)
+                result[i] = minval/distances[i]
+            end
+            result = result.^ex
+            result = result/sum(result)
+        end
+    end
+    if any(isnan,result)
+        error("Distance in kernel is too small! = ")
+    end
+    return(result)
+end
