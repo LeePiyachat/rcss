f682c335eab953a55076c0e6a911945e552c6010
Piyachat Leelasilapasart <37095103+LeePiyachat@users.noreply.github.com>
2020-03-05 11:37:00 +1100

    Delete IndEnlarge.jl

--- IndEnlarge.jl
+++
@@ -1,7 +0,0 @@
-function IndEnlarge(subgradients::Matrix{Float64},X::rcss)
-    result = Vector{Int64}(X.gnum)
-    for i in 1:X.gnum
-        result[i]=indmax(subgradients * X.Grid[i,:])
-    end
-    return(result)
-end
