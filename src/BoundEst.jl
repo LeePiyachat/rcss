201ab53ca5142f796f6d4392c280b304df34e128
Piyachat Leelasilapasart <37095103+LeePiyachat@users.noreply.github.com>
2020-03-05 11:48:33 +1100

    Update BoundEst.jl

--- src/BoundEst.jl
+++ src/BoundEst.jl
@@ -1,65 +1,66 @@
-function BoundEst(Value::Array{Float64},Evalue::Array{Float64},Initpoint::Vector{Float64},Trajectory_number::Int64,X::rcss,Index_ph::Int64,Index_va::Int64)
-    if (Index_va!= X.nnum[1]) & (Index_ph > 0)
-        println()
+function boundest(value::Array{Float64},evalue::Array{Float64},initpoint::Vector{Float64},trajectorynumber::Int64,x::rcss,index_ph::Int64,index_va::Int64)
+    if (index_va != x.nnum[1]) & (index_ph > 0)
         error("Access Index for Fast methods must match!")
     end
-    mid     = zero(Array{Float64}(undef,X.pnum,Trajectory_number))
-    high    = zero(Array{Float64}(undef,X.pnum,Trajectory_number))
-    low     = zero(Array{Float64}(undef,X.pnum,Trajectory_number))
+    mid     = zero(Array{Float64}(undef,x.pnum,trajectorynumber))
+    high    = zero(Array{Float64}(undef,x.pnum,trajectorynumber))
+    low     = zero(Array{Float64}(undef,x.pnum,trajectorynumber))
 
     #number of time steps
-    tnum = size(Value)[4]
+    tnum = size(value)[4]
 
     #running value
-    tvalue = zero(Array{Float64}(undef,X.pnum))
-    lvalue = zero(Array{Float64}(undef,X.pnum))
-    hvalue = zero(Array{Float64}(undef,X.pnum))
+    tvalue = zero(Array{Float64}(undef,x.pnum))
+    lvalue = zero(Array{Float64}(undef,x.pnum))
+    hvalue = zero(Array{Float64}(undef,x.pnum))
 
    ######
-   corrections  = zero(Array{Float64}(undef,X.pnum))
-   Evalues      = zero(Array{Float64}(undef,X.pnum))
-
+   corrections  = zero(Array{Float64}(undef,x.pnum))
+   evalues      = zero(Array{Float64}(undef,x.pnum))
+   #evalue      = zero(Array{Float64}(undef,x.pnum))
    #######
-   tvalue_new = zero(Array{Float64}(undef,X.pnum))
-   lvalue_new = zero(Array{Float64}(undef,X.pnum))
-   hvalue_new = zero(Array{Float64}(undef,X.pnum))
+   tvalue_new = zero(Array{Float64}(undef,x.pnum))
+   lvalue_new = zero(Array{Float64}(undef,x.pnum))
+   hvalue_new = zero(Array{Float64}(undef,x.pnum))
 
    #container for values
-   container_true = zero(Vector{Float64}(undef,X.anum))
-   container_low = zero(Vector{Float64}(undef,X.anum))
-   container_high = zero(Vector{Float64}(undef,X.anum))
+   container_true = zero(Vector{Float64}(undef,x.anum))
+   container_low = zero(Vector{Float64}(undef,x.anum))
+   container_high = zero(Vector{Float64}(undef,x.anum))
 
    #The loop for trajectories
-   for j in 1:Trajectory_number
+   for j in 1:trajectorynumber
        print(j); print(".")
        t = tnum
-       Pathsimulation = SimulatePath(Initpoint,tnum,1,X)
-       z = Pathsimulation[1]
-       z_labels = Pathsimulation[2]
-       for p in 1:X.pnum
-           tvalue[p] = X.Scrap(t,z[:,t],p, X.mp,"scalar") #sum(X.Scrap(t, z[:,t], p, X.mp).*z[:,t])
+       pathsimulation = simulatepath(initpoint,tnum,1,x)
+       z = pathsimulation[1]
+       z_labels = pathsimulation[2]
+       for p in 1:x.pnum
+           tvalue[p] = x.scrap(t,z[:,t],p, x.mp,"scalar") #sum(x.scrap(t, z[:,t], p, x.mp).*z[:,t])
            lvalue[p] = tvalue[p]
            hvalue[p] = lvalue[p]
        end
        while (t>1)
            t = t-1
-           corrections[:]= Get_corrections(Value,Evalue,X,z,z_labels,t,Index_ph,Index_va)
-           for pp in 1:X.pnum
-               Evalues[pp]= Get_val(Evalue[:,:,pp,t],z[:,t], X, Index_va)
+           corrections[:]= get_corrections(value,evalue,x,z,z_labels,t,index_ph,index_va)
+           for pp in 1:x.pnum
+               evalues[pp]= get_val(evalue[:,:,pp,t],z[:,t], x, index_va)
            end
-           for p in 1:X.pnum
+           for p in 1:x.pnum
                container_true   =   zero(container_true)
                container_low    =   zero(container_low)
                container_high   =   zero(container_high)
-               for a in 1:X.anum
+               for a in 1:x.anum
                    correction = 0.0
-                   for pp in 1:X.pnum
-                       container_true[a]+= X.control[p,pp,a] * Evalues[pp] #get_val(evalue[:,:,pp,t],  z[:,t], X, index_va)
-                       container_low[a] += X.control[p,pp,a] * lvalue[pp]   #get_val(value[:,:,pp,t+1],  z[:,t+1], X, index_va)
-                       container_high[a]+= X.control[p,pp,a] * hvalue[pp]
-                       correction += X.control[p,pp,a] * corrections[pp]
+                   for pp in 1:x.pnum
+                       container_true[a]+= x.positioncontrol[p,pp,a] * evalues[pp]
+                       #get_val(evalue[:,:,pp,t],  z[:,t], x, index_va)
+                       #container_true[a]+= x.control[p,pp,a] * evalue[pp]
+                       container_low[a] += x.positioncontrol[p,pp,a] * lvalue[pp]   #get_val(value[:,:,pp,t+1],  z[:,t+1], x, index_va)
+                       container_high[a]+= x.positioncontrol[p,pp,a] * hvalue[pp]
+                       correction += x.positioncontrol[p,pp,a] * corrections[pp]
                    end
-                   payoff = X.Reward(t,z[:,t],p,a,X.mp,"scalar") #sum(X.Reward(t,z[:,t],p,a,X.mp) * z[:,t])
+                   payoff = x.reward(t,z[:,t],p,a,x.mp,"scalar") #sum(x.reward(t,z[:,t],p,a,x.mp) * z[:,t])
                    container_true[a]+= payoff
                    container_low[a] += payoff + correction
                    container_high[a]+= payoff + correction
@@ -79,8 +80,8 @@ function BoundEst(Value::Array{Float64},Evalue::Array{Float64},Initpoint::Vector
        low[:,j] = lvalue[:]
        high[:,j]= hvalue[:]
    end
-   result = zero(Array{Float64}(undef,X.pnum,4, 4))
-   for p in 1:X.pnum
+   result = zero(Array{Float64}(undef,x.pnum,4, 4))
+   for p in 1:x.pnum
        #
        result[p, 1, 1] = mean(low[p,:]) #mltrack[p]
        result[p, 2, 1] = std(low[p,:])#sltrack[p]
@@ -88,25 +89,25 @@ function BoundEst(Value::Array{Float64},Evalue::Array{Float64},Initpoint::Vector
        result[p, 1, 2] = mean(high[p,:]) #mhtrack[p]
        result[p, 2, 2] = std(high[p,:])
        #
-       result[p, 1, 3] = Get_val(Value[:,:,p,1], Initpoint,X,Index_va)
+       result[p, 1, 3] = get_val(value[:,:,p,1], initpoint,x,index_va)
        result[p, 2, 3] = 0
        #
        result[p, 1, 4] = mean(mid[p,:]) #mttrack[p]
        result[p, 2, 4] = std(mid[p,:]) #sttrack[p]
    end
    for j in 1:2
-       for p in 1:X.pnum
+       for p in 1:x.pnum
            if  result[p,2 ,j] > 0
-               result[p,3:4,j]= quantile.(Normal(result[p,1,j],result[p,2,j]/sqrt(Trajectory_number)),[0.025,0.975])
+               result[p,3:4,j]= quantile.(Normal(result[p,1,j],result[p,2,j]/sqrt(trajectorynumber)),[0.025,0.975])
            else
                result[p,3:4,j]= [result[p,1 ,j], result[p,1 ,j]]
            end
        end
    end
    print("\n")
-   println("--Bound Estimation on--",Trajectory_number, "--trajectories")
+   println("--Bound Estimation on--",trajectorynumber, "--trajectories")
 
-   for p in 1:X.pnum
+   for p in 1:x.pnum
        println(" for p=", p)
        #
        println("approximate value function")
